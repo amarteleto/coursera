f90874786b0dea4fd10b501aba43a8f58202e3b9
amarteleto <amarteleto@outlook.com>
2018-09-23 20:04:57 -0300

    atualizando caso de teste do conversor.

---
+++ conversor/src/test/java/br/com/marteleto/coursera/conversor/test/AllTest.java
@@ -0,0 +1,29 @@
+package br.com.marteleto.coursera.conversor.test;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
+
+import br.com.marteleto.coursera.conversor.test.junit.ConversorUtilTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	ConversorUtilTest.class
+	//ConversorServletTest.class,
+})
+public class AllTest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	protected final transient Logger logger = Logger.getLogger(AllTest.class.getName());
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(AllTest.class);
+	    for (Failure failure : result.getFailures()) {
+	      System.out.println(failure.toString());
+	    }
+	}
+}
